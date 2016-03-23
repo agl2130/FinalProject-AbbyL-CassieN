ff1a9f14ddb4c4583b6f42907aced878ae9c03b0
cassondranealon <crn2110@tc.columbia.edu>
2016-03-23 19:52:08 -0400

    changed the name! and added a note from the other side

--- app/src/main/java/abby/finalproject_abbylcassien1/MainActivity.java
+++ app/src/main/java/abby/finalproject_abbylcassien1/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Hello from the other side!!!!!! :D
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
