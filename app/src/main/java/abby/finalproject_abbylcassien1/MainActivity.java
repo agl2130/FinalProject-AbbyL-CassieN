7ccf68130dd8cdab18a817c79ae6f94ad7e9ee31
cassondranealon <crn2110@tc.columbia.edu>
2016-03-23 20:15:56 -0400

    main act java

--- app/src/main/java/abby/finalproject_abbylcassien1/MainActivity.java
+++ app/src/main/java/abby/finalproject_abbylcassien1/MainActivity.java
@@ -1,14 +1,77 @@
 package abby.finalproject_abbylcassien1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
 
 public class MainActivity extends AppCompatActivity {
+    private Firebase rootRef;
+    private Firebase userRef;
+    private Firebase.AuthStateListener authStateListener;
 
-    //Hello from the other side!!!!!! :D
+    private EditText keyEditText;
+    private EditText valueEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        keyEditText = (EditText) findViewById(R.id.edit_text_email);
+        valueEditText = (EditText) findViewById(R.id.edit_text_password);
+
+        //set up firebase
+        Firebase.setAndroidContext(this);
+        rootRef = new Firebase("https://abbyandcassie.firebaseio.com/");
+
+        //just in case the user is logged out, do something about it
+        authStateListener = new Firebase.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(AuthData authData) {
+                if (authData != null) {
+                    //user information and their id numbers
+                    userRef = rootRef.child("users/" + authData.getUid());
+                } else {
+                    //otherwise...sorry you have to log in before you can come back
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        rootRef.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        rootRef.addAuthStateListener(authStateListener);
+    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu (Menu menu){
+//        getMenuInflater().inflate(R.menu.menu, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item){
+//        switch (item.getItemId()){
+//            case R.id.log_out:
+//                rootRef.unauth();
+//                keyEditText.setText("");
+//                valueEditText.setText("");
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
 }
