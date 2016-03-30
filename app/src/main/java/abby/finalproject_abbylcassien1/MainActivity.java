372bc62724272f7e968a1312bc4697f2dfe21501
cassondranealon <crn2110@tc.columbia.edu>
2016-03-30 19:33:30 -0400

    mA input

--- app/src/main/java/abby/finalproject_abbylcassien1/MainActivity.java
+++ app/src/main/java/abby/finalproject_abbylcassien1/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText keyEditText;
     private EditText valueEditText;
+    private int input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
