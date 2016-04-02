a0f02bed8fc9fbc8793c39116b5d13b2f11507fa
aglee <agl2130@tc.columbia.edu>
2016-04-02 15:15:28 -0400

    button casual1

--- app/src/main/java/abby/finalproject_abbylcassien1/outfitoftheday/Random.java
+++ app/src/main/java/abby/finalproject_abbylcassien1/outfitoftheday/Random.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import abby.finalproject_abbylcassien1.R;
 
@@ -19,20 +20,26 @@ public class Random extends AppCompatActivity {
     public void casual(View view) {
         Intent intent = new Intent(this, RandomTabActivity.class);
         startActivity(intent);
+
+        Toast.makeText(Random.this, "CASUAL", Toast.LENGTH_SHORT).show();
     }
 
     public void business(View view) {
         Intent intent = new Intent(this, RandomTabActivity.class);
         startActivity(intent);
+
+        Toast.makeText(Random.this, "BUSNESS", Toast.LENGTH_SHORT).show();
     }
 
     public void nightOut(View view) {
         Intent intent = new Intent(this, RandomTabActivity.class);
         startActivity(intent);
+        Toast.makeText(Random.this, "NIGHT OUT", Toast.LENGTH_SHORT).show();
     }
 
     public void surpriseMe(View view) {
         Intent intent = new Intent(this, RandomTabActivity.class);
         startActivity(intent);
+        Toast.makeText(Random.this, "SURPRISE ME!", Toast.LENGTH_SHORT).show();
     }
 }
