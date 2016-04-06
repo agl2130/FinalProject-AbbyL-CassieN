7efe56edfabcd760af0ed65afb6674c8fb005c39
aglee <agl2130@tc.columbia.edu>
2016-04-06 18:31:03 -0400

    I changed cardreader to just buttons fro now.

--- app/src/main/java/abby/finalproject_abbylcassien1/Load/AddClothes.java
+++ app/src/main/java/abby/finalproject_abbylcassien1/Load/AddClothes.java
@@ -1,11 +1,17 @@
 package abby.finalproject_abbylcassien1.Load;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 
+import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
 
+import abby.finalproject_abbylcassien1.LoginActivity;
 import abby.finalproject_abbylcassien1.R;
 
 public class AddClothes extends AppCompatActivity {
@@ -20,5 +26,63 @@ public class AddClothes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_clothes);
+
+        clothNameEditText = (EditText) findViewById(R.id.clothesEditName);
+        clothInfoEditText = (EditText) findViewById(R.id.clothesEditInfo);
+
+        Firebase.setAndroidContext(this);
+        rootRef = new Firebase("https://abbyandcassie.firebaseio.com/");
+        authStateListener = new Firebase.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(AuthData authData) {
+                if (authData != null) {
+                    // user is logged in
+                    userRef = rootRef.child("users/" + authData.getUid());
+                    return;
+                } else {
+                    // user is not logged in
+                    Intent intent = new Intent(AddClothes.this, LoginActivity.class);
+                    startActivity(intent);
+
+                }
+            }
+        };
     }
+
+    public void addToCloset(View view) {
+        Clothing clothing = new Clothing(clothNameEditText.getText().toString(), clothInfoEditText.getText().toString());
+        userRef.child("cities").push().setValue(clothing);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        rootRef.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        rootRef.removeAuthStateListener(authStateListener);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.log_out:
+                rootRef.unauth();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
 }
