74e1140fb1b21dab20a2e0a04e639825c683b0b1
cassondranealon <crn2110@tc.columbia.edu>
2016-04-06 19:24:39 -0400

    pushing

--- app/src/main/java/abby/finalproject_abbylcassien1/MainActivity.java
+++ app/src/main/java/abby/finalproject_abbylcassien1/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
     private Firebase userRef;
     private Firebase.AuthStateListener authStateListener;
     private TextView userName;
-
     private EditText keyEditText;
     private EditText valueEditText;
 
