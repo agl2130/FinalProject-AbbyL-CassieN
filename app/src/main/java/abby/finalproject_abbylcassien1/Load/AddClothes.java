0209c409beeacc08ac67680541ad2a274b93649b
aglee <agl2130@tc.columbia.edu>
2016-04-06 20:27:42 -0400

    Our addClothes work!

--- app/src/main/java/abby/finalproject_abbylcassien1/Load/AddClothes.java
+++ app/src/main/java/abby/finalproject_abbylcassien1/Load/AddClothes.java
@@ -22,6 +22,7 @@ public class AddClothes extends AppCompatActivity {
     private EditText clothNameEditText;
     private EditText clothInfoEditText;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
