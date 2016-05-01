c884dde84fdcffcaf8bba35a23560be1d0edfb2d
aglee <agl2130@tc.columbia.edu>
2016-05-01 14:13:42 -0400

    Tabs

--- app/src/main/java/abby/finalproject_abbylcassien1/walkincloset/ClothingInfoActivity.java
+++ app/src/main/java/abby/finalproject_abbylcassien1/walkincloset/ClothingInfoActivity.java
@@ -1,15 +1,31 @@
 package abby.finalproject_abbylcassien1.walkincloset;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import abby.finalproject_abbylcassien1.R;
 
 public class ClothingInfoActivity extends AppCompatActivity {
+    public String info;
+    public String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clothing_info);
+
+        Intent intent = getIntent();
+        name = intent.getStringExtra(BundleKey.NAME);
+        info = intent.getStringExtra(BundleKey.INFO);
+        int drawableId = intent.getIntExtra(BundleKey.KEY2, 0);
+        ImageView imageView = (ImageView) findViewById(R.id.prin_pic2);
+        imageView.setImageResource(drawableId);
+        fireEditText = (EditText) findViewById(R.id.info2);
+        fireEditText.setText(message);
+
+
     }
 }
