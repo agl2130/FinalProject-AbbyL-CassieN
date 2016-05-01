c7fdfca69bf7fc6893fb2cb9bb4b35cee613708d
cassondranealon <crn2110@tc.columbia.edu>
2016-05-01 16:48:20 -0400

    pushing

--- app/src/main/java/abby/finalproject_abbylcassien1/Load/AddClothes.java
+++ app/src/main/java/abby/finalproject_abbylcassien1/Load/AddClothes.java
@@ -116,21 +116,8 @@ public class AddClothes extends AppCompatActivity {
     }
 
     public void decodeUri(Uri uri) throws FileNotFoundException {
-        // Get the dimensions of the View
-//        int targetW = image.getWidth();
-//        int targetH = image.getHeight();
-//
 //        // Get the dimensions of the bitmap
         BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-//
-//        bmOptions.inJustDecodeBounds = true;
-//        BitmapFactory.decodeStream(getContentResolver().openInputStream(uri), null, bmOptions);
-//        int photoW = bmOptions.outWidth;
-//        int photoH = bmOptions.outHeight;
-//
-//        // Determine how much to scale down the image
-//        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
-
         // Decode the image file into a Bitmap sized to fill the View
         bmOptions.inJustDecodeBounds = false;
         bmOptions.inSampleSize = 1;
