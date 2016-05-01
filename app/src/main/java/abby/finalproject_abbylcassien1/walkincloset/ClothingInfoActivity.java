7df3c9fdf4dbafa1cfdbd00d6ebc0d4bfeb73345
aglee <agl2130@tc.columbia.edu>
2016-05-01 13:57:14 -0400

    Tabs

---
+++ app/src/main/java/abby/finalproject_abbylcassien1/walkincloset/ClothingInfoActivity.java
@@ -0,0 +1,13 @@
+package abby.finalproject_abbylcassien1.walkincloset;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ClothingInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clothing_info);
+    }
+}
